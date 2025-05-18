530a452846336e44765e1c3d841ec586ce5af4df
declercklouis <declerck.louis@outlook.com>
2025-05-18 23:33:36 +0200

    New updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Starting bootstrap script"
+
 # Check if the script is run as root and attempt to get privileges
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root. Attempting to get privileges..."
@@ -9,26 +11,16 @@ else
     echo "Running as root."
 fi
 
-
-echo "Starting bootstrap script"
-
-# Check whether the this repo is located in the /opt directory
 if [[ $(pwd) != "/opt/bootstrap" ]]; then
-    current_file=$(readlink -f "$0")
-    current_dir=$(pwd)
-    echo "Detected that the script is not in the /opt/bootstrap directory. "
+    echo "Detected that the script is not in the /opt/bootstrap directory."
     echo "Will reinitialize in /opt/bootstrap"
-    git clone https://github.com/declercklouis/bootstrap /opt/bootstrap
-    chmod +x /opt/bootstrap/bootstrap.sh
-    rm -rf "$current_dir/$current_file" && /opt/bootstrap/bootstrap.sh
-
+    if [[ ! -d /opt/bootstrap ]]; then
+        git clone https://github.com/declercklouis/bootstrap /opt/bootstrap
+        chmod +x /opt/bootstrap/bootstrap.sh
+    fi
+    cd /opt/bootstrap
+    exec /opt/bootstrap/bootstrap.sh "$@"
+    exit 0
 fi
 
-# CHeck if the script is run from the /opt/bootstrap directory
-if [[ $(pwd) != "/opt/bootstrap" ]]; then
-    
-    exit 1
-fi
-
-
-rm -rf current_dir
+echo "hello!
